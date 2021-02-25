b034dec823a9a8cbb1cbb4f4d5884493a723f805
Palda Puspita Dianti <paldavidi37@upi.edu>
2021-02-25 20:53:34 +0700

    commit tugas 1

---
+++ Tugas1PBO.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 20:21:04 2021
+
+@author: Palda Puspita Dianti
+"""
+
+print ("Nama  : Palda Puspita Dianti")
+print ("NIM   : 1904211")
+print ("Prodi : Sistem Telekomunikasi")
