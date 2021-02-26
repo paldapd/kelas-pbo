9b372cc7e7751fe9269b8ba4a8b130a48ac3c66d
Palda Puspita Dianti <paldavidi37@upi.edu>
2021-02-26 09:13:54 +0700

    Menambahkan file Aktifitas1.py

---
+++ Aktifitas1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:04:21 2021
+
+@author: Palda Puspita Dianti
+"""
+
+#INPUT
+nama = input ("Masukan Nama Anda: ")
+umur = 19
+
+#OUTPUT
+print ("")
+print ("=====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON=====")
+print ("Nama Anda : ", nama)
+print ("Umur Anda : ", umur)
+print ("")
