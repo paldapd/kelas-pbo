8f90c5e37956cbdcb511b4c5762ca12a8049b7a1
Palda Puspita Dianti <paldavidi37@upi.edu>
2021-02-26 09:47:49 +0700

    Menambahkan file Aktifitas3.py

---
+++ Aktifitas3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:41:36 2021
+
+@author: Palda Puspita Dianti
+"""
+
+#PERCABANGAN
+print ("Percabangan pada Bahasa Pemrograman Phyton")
+print ("Pilih Bahasa Pemrograman: ")
+print ("1. Phyton")
+print ("2. C")
+
+pilih = int (input("Pilih Menu (1/2) : "))
+
+if pilih == 1:
+    print ("Bahasa Pemrograman yang dipilih adalah Phyton")
+elif pilih == 2:
+    print ("Bahasa Pemrograman yang dipilih adalah C")
+else :
+    print ("Pilihan Anda Tidak Tersedia!!!")
