e9adcca9a301ec0316aa6f3208cf085120834cd2
Palda Puspita Dianti <paldavidi37@upi.edu>
2021-02-26 10:07:48 +0700

    Menambahkan file Aktifitas4.py

---
+++ Aktifitas4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 10:01:07 2021
+
+@author: Palda Puspita Dianti
+"""
+
+print ("Contoh Perulangan Suatu String")
+for  huruf in "SISTEL" :
+    print (huruf)
+    
+nilai_awal = 0
+nilai_akhir = 25 #tujuan/berapa kali
+step = 5 #jeda antar loop
+print ("Contoh Perulangan Angka dengan Range ()")
+for i in range (nilai_awal, nilai_akhir, step) :
+    print (i)
