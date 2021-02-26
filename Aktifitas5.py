459037ee52620f25c62b956f66ce6a8d8134bed8
Palda Puspita Dianti <paldavidi37@upi.edu>
2021-02-26 10:25:32 +0700

    Menambahkan file Aktifitas5.py

---
+++ Aktifitas5.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 10:22:12 2021
+
+@author: Palda Puspita Dianti
+"""
+
+x = int (input("Masukan Tinggi: "))
+y = 1
+
+while (y <= x) :
+    print ("*" * (y))
+    y = y + 1
